e03d5db900b53e2d97b54d6ccc84fc08e5cdc012
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-07 21:13:22 -0300

    Configura ícones

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+
+      <FontAwesomeIcon icon={faGithub} />
+    </>
   )
 }
